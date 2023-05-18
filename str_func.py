80d98b5c50346021cdf098615a61affb9e1e02db
Ildar Gabdullin <ildar8803@gmail.com>
2023-05-19 01:19:36 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def cap_letters(letters):
+    """Функция, которая делает заглавными все буквы в строке."""
     return letters.upper()
+
+
+def cap_first_letters(letters):
+    """Функция, которая делает заглавными первые буквы каждого слова в строке."""
+    return letters.title()
